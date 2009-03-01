542a8d58e8e3467e9c8c35268ecb1f274e2d0a83
Jim Weirich <jim.weirich@gmail.com>
2009-03-01 16:33:35 -0500

    t2 red

--- src/score_test.rb
+++ src/score_test.rb
@@ -6,5 +6,9 @@ class ScoreTest < Test::Unit::TestCase
   def test_empty_rolls_are_worth_zero
     assert_equal 0, score([])
   end
+
+  def test_fives_are_with_fifty
+    assert_equal 50, score([5])
+  end
   
 end
