7dde66958d77eb9901eb32b6a012cf261777cb03
Jim Weirich <jim.weirich@gmail.com>
2009-03-01 16:36:49 -0500

    t3-red

--- src/score_test.rb
+++ src/score_test.rb
@@ -7,8 +7,15 @@ class ScoreTest < Test::Unit::TestCase
     assert_equal 0, score([])
   end
 
-  def test_fives_are_with_fifty
+  def test_fives_are_worth_fifty
     assert_equal 50, score([5])
+    assert_equal 100, score([5, 5])
   end
+
+  def test_ones_are_worth_one_hundred
+    assert_equal 100, score([1])
+    assert_equal 200, score([1,1])
+  end
+  
   
 end
